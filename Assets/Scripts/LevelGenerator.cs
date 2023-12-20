6f7fdec7bd898f13a8700914b371e190c16ec24f
David Gundry <davidgundry@gmail.com>
2023-12-20 22:04:41 +0000

    Infinite worlds and post processing done quick

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [field: SerializeField]
+    public GameObject roadPrefab {get; private set;}
+
+    [field: SerializeField]
+    public GameObject[] obstaclePrefabs {get; private set;}
+
+    [field: SerializeField]
+    public GameObject player {get; private set;}
+    
+    private GameObject[] roads;
+    private List<GameObject>[] objects;
+    private int countRoads = 3;
+    private float roadLength = 200f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roads = new GameObject[countRoads];
+        objects = new List<GameObject>[countRoads];
+        for (int i=0;i<countRoads;i++)
+        {
+            GameObject road = Instantiate(roadPrefab);
+            road.transform.position = new Vector3(0, 0, roadLength*i);
+            roads[i] = road;
+            objects[i] = new List<GameObject>();
+            AddObstacles(objects[i], roadLength*i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0;i<roads.Length;i++)
+        {
+            if (roads[i].transform.position.z + roadLength < player.transform.position.z)
+            {
+                roads[i].transform.Translate(new Vector3(0, 0, roadLength*roads.Length), Space.World);
+                AddObstacles(objects[i], roads[i].transform.position.z);
+            }
+        }
+    }
+
+    void AddObstacles(List<GameObject> objects, float roadStart)
+    {
+        for (int i=0;i<objects.Count;i++)
+        {
+            Destroy(objects[i]);
+        }
+        objects.Clear();
+        int count = UnityEngine.Random.Range(5, 25);
+        for (int i=0;i<count;i++)
+        {
+            GameObject newObject = Instantiate(obstaclePrefabs[UnityEngine.Random.Range(0, obstaclePrefabs.Length)]);
+            objects.Append(newObject);
+            newObject.transform.position = new Vector3(UnityEngine.Random.Range(-8, 8), 0, roadStart + UnityEngine.Random.Range(0, roadLength));
+        }
+    }
+}
--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -16,7 +16,13 @@ public class VehicleController : MonoBehaviour
     [field: SerializeField, Range(0f, 40f), Tooltip("Change in velocity per second")]
     public float acceleration {get; private set;}
 
-    [field: SerializeField, Range(0f, 0.01f)]
+    [field: SerializeField, Range(0f, 40f)]
+    public float breakAcceleration {get; private set;}
+
+    [field: SerializeField, Range(0f, 40f)]
+    public float reverseAcceleration {get; private set;}
+
+    [field: SerializeField, Range(0f, 0.03f)]
     public float friction { get; private set;}
 
     [field: SerializeField, Range(0f, 60f)]
@@ -25,7 +31,7 @@ public class VehicleController : MonoBehaviour
     [field: SerializeField, Range(0f, 60f)]
     public float wheelTurnAngle { get; private set;}
 
-    [field: SerializeField, Range(0f, 20f), Header("Read only")]
+    [field: SerializeField, Range(0f, 40f), Header("Read only")]
     public float speed { get; private set;}
 
     // Start is called before the first frame update
@@ -40,21 +46,29 @@ public class VehicleController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-
-
         float turnAmount = horizontalInput * EaseOutQuintic(Math.Abs(speed/maxSpeed));
+        if (transform.position.x < -8)
+            turnAmount = Math.Max(turnAmount, (-8-transform.position.x)/5);
+        if (transform.position.x > 8)
+            turnAmount = Math.Min(turnAmount, -(transform.position.x -8)/5);
         if (speed < 0)
             turnAmount = -turnAmount;
         transform.Rotate(Vector3.up * Time.deltaTime  * turnSpeed * turnAmount);
 
-        speed += Time.deltaTime * acceleration * verticalInput;
+        if (verticalInput > 0)
+            speed += Time.deltaTime * acceleration * verticalInput;
+        if (speed > 0 && verticalInput < 0)
+            speed += Time.deltaTime * breakAcceleration * verticalInput;
+        if (speed <= 0 && verticalInput < 0)
+            speed += Time.deltaTime * reverseAcceleration * verticalInput;
         speed *= 1-friction;
         speed = Math.Min(maxSpeed, speed);
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
-        float wheelRadius = 1;
+        
 
+        float wheelRadius = 1;
         for (int i=0;i<wheels.Length;i++)
             wheels[i].transform.Rotate(Vector3.right * (Time.deltaTime * speed / (wheelRadius * Mathf.PI*2)) * 360);
         for (int i=0;i<2;i++)
